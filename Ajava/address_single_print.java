5a2508483a4dd3f0710b306b613a6439f3d641c9
Ajaykumarrns <88618812+Ajaykumarrns@users.noreply.github.com>
2025-08-06 16:28:46 +0530

    Add files via upload

---
+++ Ajava/address_single_print.java
@@ -0,0 +1,8 @@
+//wap to print your address using single print
+public class address_single_print {
+    public static void main(String[] args) {
+        System.out.print("MY ADDRESSS IS GR.NOIDA, UP, INDIA");
+    }
+    
+}
+
